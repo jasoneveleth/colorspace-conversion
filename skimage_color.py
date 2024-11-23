f49cb71936d7b8c294a3444ad8f0c7497cf30d49
Jason Eveleth <jj11@eveleth.com>
2024-11-22 19:58:19 -0500

    .

--- skimage_color.py
+++ skimage_color.py
@@ -3,3 +3,5 @@ import skimage
 rgb2lab = lambda x: skimage.color.rgb2lab([[x]])[0,0,:]
 lab2rgb = lambda x: skimage.color.lab2rgb([[x]])[0,0,:]
 
+import numpy as np
+print(lab2rgb(np.array([73.0, -29.494755099535887, 27.01961179621946])))
